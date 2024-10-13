f56f1473e951be289c9e70ed28af76f48106acad
David Coles <dcoles@dcoles.net>
2024-10-13 02:14:08 -0700

    Generate bindings at compile-time
    This replaces the `generate-bindings` script with a `build.rs` script
    for `flipperzero-sys` that downloads the latest SDK and uses that to
    generate Rust bindings.
    
    Fixes: #41

--- crates/sys/src/lib.rs
+++ crates/sys/src/lib.rs
@@ -18,24 +18,9 @@
 )))]
 core::compile_error!("This crate requires `--target thumbv7em-none-eabihf`");
 
+mod bindings;
 pub mod furi;
 mod inlines;
-
-#[allow(
-    non_upper_case_globals,
-    non_camel_case_types,
-    non_snake_case,
-    clippy::missing_safety_doc,
-    clippy::transmute_int_to_bool,
-    clippy::useless_transmute,
-    rustdoc::broken_intra_doc_links,
-// https://github.com/rust-lang/rust-bindgen/issues/2807
-    unnecessary_transmutes,
-// https://github.com/rust-lang/rust-bindgen/issues/3053
-    clippy::ptr_offset_with_cast,
-)]
-#[rustfmt::skip]
-mod bindings;
 
 /// Crash the system.
 ///
