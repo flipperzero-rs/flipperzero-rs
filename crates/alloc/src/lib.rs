e374cb94029d8378d0ac015440e63f64b1fb2c71
Lofty Inclination <alastaircodd@hotmail.co.uk>
2026-01-03 22:43:51 +0000

    Replace the global allocator's sys calls with miri extern helpers

--- crates/alloc/src/lib.rs
+++ crates/alloc/src/lib.rs
@@ -6,10 +6,12 @@
 
 use core::alloc::{GlobalAlloc, Layout};
 
+#[cfg(not(miri))]
 use flipperzero_sys as sys;
 
 pub struct FuriAlloc;
 
+#[cfg(not(miri))]
 unsafe impl GlobalAlloc for FuriAlloc {
     #[inline]
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
@@ -25,6 +27,25 @@ unsafe impl GlobalAlloc for FuriAlloc {
     unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
         // https://github.com/flipperdevices/flipperzero-firmware/issues/1747#issuecomment-1253636552
         unsafe { self.alloc(layout) }
+    }
+}
+
+#[cfg(miri)]
+unsafe extern "Rust" {
+    fn miri_alloc(size: usize, align: usize) -> *mut u8;
+    fn miri_dealloc(ptr: *mut u8, size: usize, align: usize);
+}
+
+#[cfg(miri)]
+unsafe impl GlobalAlloc for FuriAlloc {
+    #[inline]
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        unsafe { miri_alloc(layout.size(), layout.align()) }
+    }
+
+    #[inline]
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        unsafe { miri_dealloc(ptr, layout.size(), layout.align()) };
     }
 }
 
