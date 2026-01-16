5437ab71faadf25cb8e07ea1f98e274d7788820c
Lofty Inclination <alastaircodd@hotmail.co.uk>
2026-01-16 00:07:18 +0000

    Implement some more stuff
    current version of the incantation:
    MIRIFLAGS=-Zmiri-backtrace=full cargo +miri miri run --target thumbv7em-none-eabihf --example gui

--- crates/sys/src/miri_bindings/gui.rs
+++ crates/sys/src/miri_bindings/gui.rs
@@ -6,6 +6,8 @@ mod view_dispatcher;
 mod view_port;
 mod widget;
 
+extern crate alloc;
+
 pub use canvas::*;
 pub use icon::*;
 pub use text_input::*;
@@ -14,7 +16,13 @@ pub use view_dispatcher::*;
 pub use view_port::*;
 pub use widget::*;
 
+use crate::InputEvent;
 use crate::miri_bindings::utils::*;
+use alloc::sync::Arc;
+use core::cell::UnsafeCell;
+use core::ops::{Deref, DerefMut};
+use core::ptr;
+use core::sync::atomic::{AtomicBool, AtomicPtr, Ordering};
 
 #[doc = "Set lockdown mode\n\n When lockdown mode is enabled, only GuiLayerDesktop is shown.\n This feature prevents services from showing sensitive information when flipper is locked.\n\n # Arguments\n\n* `gui` - Gui instance\n * `lockdown` - bool, true if enabled"]
 pub unsafe fn gui_set_lockdown(gui: *mut Gui, lockdown: bool) {
@@ -48,11 +56,135 @@ pub const GuiLayerMAX: GuiLayer = GuiLayer(5);
 #[doc = "Gui layers"]
 #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
 pub struct GuiLayer(pub core::ffi::c_uchar);
-#[repr(C)]
-#[derive(Debug, Copy, Clone)]
-pub struct Gui {
-    _unused: [u8; 0],
+
+mod lock {
+    use crate::miri_bindings::utils::*;
+    use core::cell::UnsafeCell;
+    use core::ops::{Deref, DerefMut};
+    use core::sync::atomic::{AtomicBool, AtomicPtr, Ordering};
+
+    pub struct SpinLock<T> {
+        data: UnsafeCell<T>,
+        inner: AtomicBool,
+    }
+
+    pub struct SpinLockGuard<'a, T> {
+        lock: &'a SpinLock<T>,
+    }
+
+    impl<T> SpinLock<T> {
+        pub fn new(data: T) -> Self {
+            Self {
+                data: data.into(),
+                inner: AtomicBool::new(false),
+            }
+        }
+
+        pub fn lock(&self) -> SpinLockGuard<'_, T> {
+            // NOTE: SeqCst has been used all over here, bcs it's definitely correct, and I haven't got
+            // a good enough handle on the other orderings to pick one that would also be correct but
+            // more efficient.
+            while !self
+                .inner
+                .compare_exchange_weak(false, true, Ordering::SeqCst, Ordering::SeqCst)
+                .is_ok()
+            {
+                miri_spin_loop();
+            }
+            SpinLockGuard { lock: self }
+        }
+    }
+
+    impl<'a, T> Deref for SpinLockGuard<'a, T> {
+        type Target = T;
+
+        fn deref(&self) -> &T {
+            unsafe { &*self.lock.data.get() }
+        }
+    }
+
+    impl<'a, T> DerefMut for SpinLockGuard<'a, T> {
+        fn deref_mut(&mut self) -> &mut T {
+            unsafe { &mut *self.lock.data.get() }
+        }
+    }
+
+    impl<'a, T> Drop for SpinLockGuard<'a, T> {
+        fn drop(&mut self) {
+            // NOTE: SeqCst has been used all over here, bcs it's definitely correct, and I haven't got
+            // a good enough handle on the other orderings to pick one that would also be correct but
+            // more efficient.
+            self.lock.inner.store(false, Ordering::SeqCst);
+        }
+    }
 }
+
+#[repr(C)]
+pub struct Gui {
+    thread_id: usize,
+    input_channel: Option<InputEvent>,
+    request_redraw: bool,
+    stop: bool,
+}
+
+impl Gui {
+    // This isn't _entirely_ correct to the source; in that, the GUI record is created and
+    // populated by the GUI svc thread, not the other way around.
+    pub fn spawn() -> Arc<lock::SpinLock<Self>> {
+        let gui = Self {
+            thread_id: 0,
+            input_channel: None,
+            request_redraw: false,
+            stop: false,
+        };
+        let mut gui = Arc::new(lock::SpinLock::new(gui));
+
+        let thread_id = {
+            let gui = gui.clone();
+            let gui_ptr = Arc::into_raw(gui);
+            // SAFETY: Arc was generated above
+            unsafe { miri_thread_spawn(thread_start, gui_ptr as *mut _) }
+        };
+
+        {
+            gui.lock().thread_id = thread_id;
+        }
+
+        extern "Rust" fn thread_start(data: *mut ()) {
+            // SAFETY: data is guaranteed to have been created from an arc, just above
+            let gui: Arc<lock::SpinLock<Gui>> = unsafe { Arc::from_raw(data as *const _) };
+
+            loop {
+                let gui = &mut gui.lock();
+
+                if let Some(input) = gui.input_channel.take() {
+                    gui.process_input(input);
+                }
+
+                if gui.request_redraw {
+                    gui.redraw();
+                }
+
+                if gui.stop {
+                    break;
+                }
+
+                miri_spin_loop();
+            }
+        }
+
+        gui
+    }
+
+    fn process_input(&self, input: InputEvent) -> () {
+        todo!();
+    }
+
+    fn redraw(&self) -> () {
+        todo!()
+    }
+}
+
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
 pub struct View {
@@ -77,5 +209,9 @@ pub const GuiButtonTypeRight: GuiButtonType = GuiButtonType(2);
 #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
 pub struct GuiButtonType(pub core::ffi::c_uchar);
 pub type ButtonCallback = ::core::option::Option<
-    unsafe extern "C" fn(result: GuiButtonType, type_: crate::InputType, context: *mut core::ffi::c_void),
+    unsafe extern "C" fn(
+        result: GuiButtonType,
+        type_: crate::InputType,
+        context: *mut core::ffi::c_void,
+    ),
 >;
